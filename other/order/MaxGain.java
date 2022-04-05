12869265cf929e2d77bea3c78b483b455170de9d
CHADAD <chadad.mohamed@gmail.com>
2022-04-05 13:18:39 +0100

    Solve AmineBenAddi task

--- other/order/MaxGain.java
+++ other/order/MaxGain.java
@@ -7,6 +7,21 @@ for exemple in this list it's 80 - 1
 
 class MaxGain {
     public static void main(String[] args) {
-        System.out.println()
+        System.out.println(maxGain(new int[]{2, 3, 10, 1, 12, 80, 6, 8, 30}));
+    }
+
+    public static int maxGain(int[] prices) {
+        int res = 0;
+        int a = 0, b = 1;
+        while (a < b && b < prices.length) {
+            if (prices[a] < prices[b]) {
+                res = Math.max(prices[b] - prices[a], res);
+                b++;
+            } else {
+                a = b;
+                b = a + 1;
+            }
+        }
+        return res;
     }
 }
