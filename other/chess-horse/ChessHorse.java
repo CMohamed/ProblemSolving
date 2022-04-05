d41d1398bb351480df7871fbf7616959a0baf066
CHADAD <chadad.mohamed@gmail.com>
2022-04-05 15:25:11 +0100

    Horse moves Taoufik ajaanit prob WIP

---
+++ other/chess-horse/ChessHorse.java
@@ -0,0 +1,15 @@
+/*
+Given the coordinates of the current pos of the horse r, c and coordinates of the target position
+
+what is the minimum moves that should the horse do to reach the target
+*/
+
+class ChessHorse {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minHorseMoves(int startR, int startC, int targetR, int targetC) {
+
+    }
+}
