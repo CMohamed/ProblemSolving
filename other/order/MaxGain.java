9d671b198ffc11d4b46ca4ceb9995c447bbceaec
CHADAD <chadad.mohamed@gmail.com>
2022-04-05 12:10:39 +0100

    MaxGain by AmineBenaddi WIP

---
+++ other/order/MaxGain.java
@@ -0,0 +1,12 @@
+/*
+Prices = [2, 3, 10, 1, 12, 80, 6, 8, 30]
+find the highest profite from this list in a linear time window
+for exemple in this list it's 80 - 1
+*/
+
+
+class MaxGain {
+    public static void main(String[] args) {
+        System.out.println()
+    }
+}
