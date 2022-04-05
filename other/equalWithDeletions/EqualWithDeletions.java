ed4f1ed67d7de9788a34f11655c17553722d30dd
CHADAD <chadad.mohamed@gmail.com>
2022-04-05 01:52:21 +0100

    add problem desciption

--- other/equalWithDeletions/EqualWithDeletions.java
+++ other/equalWithDeletions/EqualWithDeletions.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
+/**
+Given two strings n and m, return true if they are equal when both are entered into text editors.
+But: a # means a backspace character (deleting backwards), and a % means a delete character (deleting forwards).
+Example:
+> equalWithDeletions("a##x", "#a#x")
+> true      // both strings become "x"
+> equalWithDeletions("fi##f%%%th %%year #time###", "fifth year time")
+> false     // the first string becomes "fart"
+ */
+
+
 public class EqualWithDeletions {
     
     public static void main(String[] args) {
